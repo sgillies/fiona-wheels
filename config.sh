615b1e680f2b8adc10ed40f4f101a4ba0c272f38
Sean Gillies <sean@mapbox.com>
2021-05-28 17:04:20 -0600

    Changes required by GDAL 3.3

--- config.sh
+++ config.sh
@@ -232,7 +232,7 @@ function run_tests {
         sudo apt-get install -y ca-certificates
     fi
     cp -R ../Fiona/tests ./tests
-    python -m pytest -vv tests -k "not test_collection_zip_http and not test_mask_polygon_triangle and not test_show_versions"
+    GDAL_ENABLE_DEPRECATED_DRIVER_GTM=YES python -m pytest -vv tests -k "not test_collection_zip_http and not test_mask_polygon_triangle and not test_show_versions and not test_append_or_driver_error[DGN]"
     fio --version
     fio env --formats
     pip install shapely && python ../test_fiona_issue383.py
