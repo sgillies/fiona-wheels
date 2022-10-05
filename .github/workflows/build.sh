63e4216bd448e53df514ec8ede0f143531de0b1f
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2022-10-05 15:26:35 +0200

    remove if for test wheels

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,27 +4,22 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   # curl from brew requires zstd, use system curl
   # if php is installed, brew tries to reinstall these after installing openblas
   brew remove --ignore-dependencies webp zstd xz libtiff curl php
-  PATH=/opt/homebrew/opt/python@3.9/libexec/bin:$PATH
 fi
 
-echo "::group::Install a virtualenv"
-  PATH=/opt/homebrew/opt/python@3.9/libexec/bin:$PATH
+echo "::group::Build wheel"
+  PATH=/opt/homebrew/opt/python@${MB_PYTHON_VERSION}/libexec/bin:$PATH
   python -c "import sys; print(sys.version)" | awk -F \. {'print $1$2'}
-  sleep 5
   source multibuild/common_utils.sh
   source multibuild/travis_steps.sh
-  #python3 -m pip install virtualenv
-  #before_install
-echo "::endgroup::"
-
-echo "::group::Build wheel"
   clean_code $REPO_DIR $BUILD_COMMIT
   build_wheel $REPO_DIR $PLAT
   ls -l "${GITHUB_WORKSPACE}/${WHEEL_SDIR}/"
 echo "::endgroup::"
 
-if [[ $MACOSX_DEPLOYMENT_TARGET != "11.0" ]]; then
-  echo "::group::Test wheel"
-    install_run $PLAT
-  echo "::endgroup::"
-fi
+echo "::group::Test wheel"
+  PATH=/opt/homebrew/opt/python@${MB_PYTHON_VERSION}/libexec/bin:$PATH
+  source multibuild/common_utils.sh
+  echo $PIP_CMD
+  install_run $PLAT
+echo "::endgroup::"
+
